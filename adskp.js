3790c1fd1bc31eb62292c76015bcd134da590a71
pembodohan89 <30522574+pembodohan89@users.noreply.github.com>
2017-10-09 16:46:11 +0700

    Update adskp.js

--- adskp.js
+++ adskp.js
@@ -1,4 +1,4 @@
 function wa(btn,txt_btn,txt_msg,phone){
- url="https://api.pkkumb.com/btn.php?btn="+btn+"&txt_btn="+txt_btn+"&txt_msg="+txt_msg+"&phone="+phone; 
+ url="https://raw.githubusercontent.com/pembodohan89/new/master/btn.php?btn="+btn+"&txt_btn="+txt_btn+"&txt_msg="+txt_msg+"&phone="+phone; 
 	document.getElementById("insEl").innerHTML='<iframe height="50" src="'+url+'" frameborder="0" marginwidth="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allowfullscreen="true"></iframe>';
 }
