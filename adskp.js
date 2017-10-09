6fab6754bcb15f1421bc75a175b810287c8d458f
pembodohan89 <30522574+pembodohan89@users.noreply.github.com>
2017-10-09 16:47:05 +0700

    Delete adskp.js

--- adskp.js
+++
@@ -1,4 +0,0 @@
-function wa(btn,txt_btn,txt_msg,phone){
- url="https://raw.githubusercontent.com/pembodohan89/new/master/btn.php?btn="+btn+"&txt_btn="+txt_btn+"&txt_msg="+txt_msg+"&phone="+phone; 
-	document.getElementById("insEl").innerHTML='<iframe height="50" src="'+url+'" frameborder="0" marginwidth="0" vspace="0" hspace="0" allowtransparency="true" scrolling="no" allowfullscreen="true"></iframe>';
-}
