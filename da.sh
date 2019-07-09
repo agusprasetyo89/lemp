1cbd296e31274bb43f85815a24c5a9a109a2958e
pembodohan89 <30522574+pembodohan89@users.noreply.github.com>
2019-07-09 14:05:31 +0700

    Update da.sh

--- da.sh
+++ da.sh
@@ -12,7 +12,7 @@ service firewalld stop;
 systemctl disable firewalld;
 yum install wget gcc gcc-c++ flex bison make bind bind-libs bind-utils openssl openssl-devel perl quota libaio libcom_err-devel libcurl-devel gd zlib-devel zip unzip libcap-devel cronie bzip2 cyrus-sasl-devel perl-ExtUtils-Embed autoconf automake libtool which patch mailx bzip2-devel lsof glibc-headers kernel-devel expat-devel -y;yum install psmisc net-tools systemd-devel libdb-devel perl-DBI perl-Perl4-CoreLibs xfsprogs rsyslog logrotate crontabs file kernel-headers -y;wget https://www.directadmin.com/setup.sh;bash setup.sh;
 exit
-			;
+			
       2)
             echo 'letsencrypt=1'>>/usr/local/directadmin/conf/directadmin.conf;
             service directadmin restart;
@@ -23,11 +23,11 @@ exit
             ./build letsencrypt;
             service directadmin restart;
       exit
-			;
+			
       3)
 
 echo -ne "Please type your hostname: "
 read HOSTNAME
 cd /usr/local/directadmin/scripts;./letsencrypt.sh request_single $HOSTNAME 4096;cd /usr/local/directadmin/conf;perl -pi -e 's/SSL=0/SSL=1/' directadmin.conf;echo "carootcert=/usr/local/directadmin/conf/carootcert.pem" >> directadmin.conf;echo "force_hostname=$HOSTNAME" >> directadmin.conf;echo "ssl_redirect_host=$HOSTNAME" >> directadmin.conf;service directadmin restart;
    exit
-			;
+			
